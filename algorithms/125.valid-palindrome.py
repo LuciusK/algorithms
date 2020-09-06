1f0aa14aec75ea45be271acffc9bdcc604c5cc3b
Lucius <LuciusPrivate@163.com>
2020-09-06 19:38:28 +0800

    9/6

---
+++ algorithms/125.valid-palindrome.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=125 lang=python3
+#
+# [125] Valid Palindrome
+#
+
+# @lc code=start
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        sgood = "".join(ch.lower() for ch in s if ch.isalnum())
+        return sgood == sgood[::-1]
+
+    def isPalindrome1(self, s: str) -> bool:
+        n = len(s)
+        left, right = 0, n - 1
+
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if left < right:
+                if s[left].lower() != s[right].lower():
+                    return False
+                left, right = left + 1, right - 1
+        return True
+        
+# @lc code=end
+
---
+++ algorithms/917.reverse-only-letters.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=917 lang=python3
+#
+# [917] Reverse Only Letters
+#
+
+# @lc code=start
+class Solution:
+    def reverseOnlyLetters(self, S: str) -> str:
+        letters = [c for c in S if c.isalpha()]
+        ans = []
+        for c in S:
+            if c.isalpha():
+                ans.append(letters.pop())
+            else:
+                ans.append(c)
+        return "".join(ans)
+    
+    def reverseOnlyLetters1(self, S: str) -> str:
+        ans = []
+        j = len(S) - 1
+        for i, x in enumerate(S):
+            if x.isalpha():
+                while not S[j].isalpha():
+                    j -= 1
+                ans.append(S[j])
+                j -= 1
+            else:
+                ans.append(x)
+        return "".join(ans)
+
+        
+# @lc code=end
+
