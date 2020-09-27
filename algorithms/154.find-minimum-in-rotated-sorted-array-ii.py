181c079d6e3eb520e102eb5d007b4387e66c4673
Lucius <LuciusPrivate@163.com>
2020-09-27 23:00:56 +0800

    9/27

---
+++ algorithms/154.find-minimum-in-rotated-sorted-array-ii.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=154 lang=python3
+#
+# [154] Find Minimum in Rotated Sorted Array II
+#
+
+# @lc code=start
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) >> 1
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            elif nums[mid] < nums[right]:
+                right = mid
+            else:
+                right -= 1
+        return nums[left]
+# @lc code=end
+
---
+++ algorithms/74.search-a-2-d-matrix.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=74 lang=python3
+#
+# [74] Search a 2D Matrix
+#
+
+# @lc code=start
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        if m == 0:
+            return False
+        n = len(matrix[0])
+        
+        #二分查找
+        left, right = 0, m * n - 1
+        while left <= right:
+                pivot_idx = (left + right) // 2
+                pivot_element = matrix[pivot_idx // n][pivot_idx % n]
+                if target == pivot_element:
+                    return True
+                else:
+                    if target < pivot_element:
+                        right = pivot_idx - 1
+                    else:
+                        left = pivot_idx + 1
+        return False
+
+# @lc code=end
+
