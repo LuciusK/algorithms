831a72ff64e1d2bcce08e35ea558582ef7d2cf94
Lucius <LuciusPrivate@163.com>
2021-02-27 16:28:07 +0800

    new

--- algorithms/15.3-sum.py
+++ algorithms/15.3-sum.py
@@ -16,25 +16,25 @@ class Solution:
         for k in range(n - 2):
             if nums[k] > 0:
                 return res
-            if k > 0 and nums[k] == nums[k-1]:
+            if k > 0 and nums[k] == nums[k - 1]:
                 continue
             i = k + 1
             j = n - 1
             while i < j:
                 if nums[k] + nums[i] + nums[j] == 0:
                     res.append([nums[k], nums[i], nums[j]])
-                    while i < j and nums[i] == nums[i+1]:
+                    while i < j and nums[i] == nums[i + 1]:
                         i += 1
-                    while i < j and nums[j] == nums[j-1]:
+                    while i < j and nums[j] == nums[j - 1]:
                         j -= 1
                     i += 1
                     j -= 1
                 elif nums[k] + nums[i] + nums[j] > 0:
-                    while i < j and nums[j] == nums[j-1]:
+                    while i < j and nums[j] == nums[j - 1]:
                         j -= 1
                     j -= 1
                 else:
-                    while i < j and nums[i] == nums[i+1]:
+                    while i < j and nums[i] == nums[i + 1]:
                         i += 1
                     i += 1
         return res
--- algorithms/18.4-sum.py
+++ algorithms/18.4-sum.py
@@ -12,28 +12,28 @@ class Solution:
             return []
         nums.sort()
         for k in range(n - 3):
-            if k > 0 and nums[k] == nums[k-1]:
+            if k > 0 and nums[k] == nums[k - 1]:
                 continue
-            for l in range(k+1, n-2):
-                if l > k+1 and nums[l] == nums[l-1]:
+            for l in range(k + 1, n - 2):
+                if l > k + 1 and nums[l] == nums[l - 1]:
                     continue
                 i = l + 1
                 j = n - 1
                 while i < j:
                     if nums[k] + nums[l] + nums[i] + nums[j] == target:
                         res.append([nums[k], nums[l], nums[i], nums[j]])
-                        while i < j and nums[i] == nums[i+1]:
+                        while i < j and nums[i] == nums[i + 1]:
                             i += 1
-                        while i < j and nums[j] == nums[j-1]:
+                        while i < j and nums[j] == nums[j - 1]:
                             j -= 1
                         i += 1
                         j -= 1
                     elif nums[k] + nums[l] + nums[i] + nums[j] > target:
-                        while i < j and nums[j] == nums[j-1]:
+                        while i < j and nums[j] == nums[j - 1]:
                             j -= 1
                         j -= 1
                     else:
-                        while i < j and nums[i] == nums[i+1]:
+                        while i < j and nums[i] == nums[i + 1]:
                             i += 1
                         i += 1
         return res
